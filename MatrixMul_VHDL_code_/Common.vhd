c49d2f384f13060793ae475234c0898884cb0af2
giadaanastasi <40028778+giadaanastasi@users.noreply.github.com>
2020-01-26 19:07:01 +0100

    code & report

---
+++ MatrixMul_VHDL_code_/Common.vhd
@@ -0,0 +1,11 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;        -- for addition & counting
+use ieee.numeric_std.all;               -- for type conversions
+
+package Common is
+	type BitMatrix is array(natural range<>,natural range<>) of std_ulogic_vector;
+end Common;
+
+package body Common is
+end Common;
